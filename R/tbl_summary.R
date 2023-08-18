6691246db43e9e3b79d616d8a16a912286029dc1
Thomas Bøjer Rasmussen <thomasboejer@gmail.com>
2023-08-18 21:12:42 +0200

    Add .n variable

--- R/tbl_summary.R
+++ R/tbl_summary.R
@@ -1,7 +1,7 @@
 # Quiet R CMD check notes of the type "no visible binding for global variable..."
 utils::globalVariables(c(
   ".n_by_level", ".p25", ".p50", ".p75", ".stat_label", ".stat_value",
-  ".sum", ".var_type"
+  ".sum", ".var_type", ".n"
 ))
 
 #' Summarize variables
@@ -33,8 +33,13 @@ tbl_summary <- function(x,
   # TODO: need to add something here
 
   ### Summarize variables
+
+  # Add "n" variable to data
+  x1 <- data.table::as.data.table(x)[, .n := 1L]
+  vars <- c(".n", vars)
+
   dt <- summarize_df(
-    x,
+    x1,
     vars = vars,
     weight = weight,
     by = by,
@@ -50,7 +55,7 @@ tbl_summary <- function(x,
 
   ### Calculate stats ###
 
-  # TODO: should be put in utility function?
+  # TODO: should be put in flexible utility function?
 
   dt <- dt[
     , `:=`(.stat_label = fcase(
@@ -77,6 +82,22 @@ tbl_summary <- function(x,
     )
   ]
 
+  # Fix ".n" statistic
+  # TODO: this should be handled automatically instead. Mybe add ".n" variable type
+  # or is there a better way? That type would be exposed to users...
+  dt <- dt[
+    , `:=` (
+      .var_type = fifelse(.var_name == ".n", "n", .var_type),
+      .var_level = fifelse(.var_name == ".n", "", .var_level),
+      .stat_label = fifelse(.var_name == ".n", "n", .stat_label ),
+      .stat_value = fifelse(
+        .var_name == ".n",
+        unlist(strsplit(.stat_value, split = " "))[1:(nrow(dt)*2) %% 2 == 1L],
+        .stat_value
+      )
+    )
+  ]
+
   dt <- dt[
     , .(.by, .var_name, .var_type, .n_by_level, .var_level,
         .stat_label, .stat_value, .mask_by_level, .mask_var_level)
@@ -90,7 +111,7 @@ tbl_summary <- function(x,
         .stat_value
       )
     ][
-    , `:=`(.mask_by_level = NULL, .mask_var_level = NULL)
+    , `:=`(.mask_by_level = NULL, .mask_var_level = NULL, .n_by_level = NULL)
   ]
 
 
