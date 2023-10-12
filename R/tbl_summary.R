b3512b6242a843982f5f1a6b7ed6a50983b9e7cf
Thomas Bøjer Rasmussen <thomasboejer@gmail.com>
2023-10-12 19:52:51 +0200

    Update tbl_summary documentation

--- R/tbl_summary.R
+++ R/tbl_summary.R
@@ -4,23 +4,59 @@ utils::globalVariables(c(
   ".sum", ".var_type", ".n", ".stat_num1", ".stat_num2", ".stat_num3"
 ))
 
-#' Summarize variables
+#' Make descriptive summary
 #'
-#' @param x a
-#' @param vars a
-#' @param weight a
-#' @param by a
-#' @param by_total_val a
+#' Makes a descriptive summary of the distribution of variables in a given
+#' data.frame.
+#'
+#' If the type of a variable in `vars` is not specified, the type is deduced
+#' from the data using the following algorithm:
+#' 1) If the variable takes "binary" values, ie is a logical vector, or a
+#' numeric vector with zero and one's, then the variable is assumed to be
+#' a binary variable.
+#' 2) Else, if the variable is numeric then it is assumed to be a
+#' continuous variable.
+#' 3) Else, the variable is assumed to be a categorical variable.
+
+#' @param x A data.frame. Data to summarize.
+#' @param vars Character vector. Names of variables in `x` to summarize.
+#'   Variables types can be specified by naming elements of `var`. Variable
+#'   types are: "bin" (binary), "cont" (continuous), "cat" (categorical).
+#'   If no variable type is specified, the type is automatically deduced from
+#'   the data. See details for more information.
+#' @param weight String. Name of variable in `x` that contains
+#'   observation weights. Must be a numeric variable, with non-negative
+#'   real values. By default (`weight` = NULL), all weights are set to one.
+#' @param by String. Name of variable to group by while summarizing. Default is
+#'   NULL, ie no grouping variable is used. Note that if no grouping variable is
+#'   used, the returned data.table will still contain a ".by" variable, taking
+#'   the value given in `by_total_val`.
+#' @param by_total_val String. Name given to overall by-group.
 #' @param mask A logical value. Mask person-sensitive aggregated counts?
 #' @param add_num_stat A logical value. Add numeric columns with statistics?
 #'
-#' @return data.table
+#' @return A data.table containing a descriptive summary of `vars`. The
+#'   data.table contains the following variables:
+#'   - ".by": by-group value.
+#'   - ".var_name": Variable name.
+#'   - ".var_type": Variable type.
+#'   - ".var_level": Variable level. Only used for categorical variables.
+#'   - ".stat_label": Short-name of calculated statistics.
+#'   - ".stat_value": Statistics value.
+#'
 #' @export
 #'
 #' @examples
-#' \dontrun{
-#'   TODO
-#' }
+#' x <- data.frame(
+#'   var_cont = 1:10,
+#'   var_bin = c(rep(1, 5), rep(0, 5)),
+#'   var_cat = c(rep(1, 5), rep(2, 5))
+#' )
+#'
+#' dt <- tbl_summary(
+#'   x = x,
+#'   vars = c("var_cont", "var_bin", cat = "var_cat"),
+#' )
 tbl_summary <- function(x,
                         vars,
                         weight = NULL,
